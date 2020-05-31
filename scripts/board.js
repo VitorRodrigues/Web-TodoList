755518989ca2d7010a70d13a6ab91cadec0cb31f
Vitor Rodrigues <vitor.n.rodrigues@gmail.com>
2020-05-31 19:25:47 -0300

    Alpha version commit with drag and drop feature

---
+++ scripts/board.js
@@ -0,0 +1,33 @@
+var board = {
+    name: "Board da vida webdev",
+    columns: [{
+            id: "todo",
+            title: "To Doo",
+            cards: [{
+                    id: "3",
+                    title: "Publicar"
+                },
+                {
+                    id: "5",
+                    title: "Divulgar"
+                }
+            ]
+        },
+        {
+            id: "doing",
+            title: "Doingg",
+            cards: [{
+                id: "2",
+                title: "Aprender"
+            }]
+        },
+        {
+            id: "done",
+            title: "Donne",
+            cards: [{
+                id: "1",
+                title: "Procrastinar"
+            }]
+        }
+    ]
+}
---
+++ scripts/renderer.js
@@ -0,0 +1,53 @@
+// import "board.js"
+
+Vue.component("board", {
+    props: ["board"],
+    template: `
+    <div id="board">
+    <h1>{{ board.name }}</h1>
+    <div id="actions">
+        <button class="btn btn-primary" aria-label="Adicionar tarefa">Nova tarefa</button>
+        <button class="btn btn-secondary right" aria-label="Excluir">Excluir</button>
+    </div>
+    <div id="board" class="board-layout">
+        <column-list v-bind:columns="board.columns"></column-list>
+    </div>
+    </div>`
+})
+
+Vue.component("column-list", {
+    props: ["columns"],
+    template: '<div class="columns-list">\
+     <card-column v-for="col in columns" v-bind:key="col.id" v-bind:column="col"></card-column> \
+     </div>'
+})
+
+Vue.component('card-column', {
+    props: ['column'],
+    template: '<div :id="column.id" class="column"\
+    ondrop="dropIt(event)"\
+    ondragover="allowDrop(event)"\
+    ondragenter="dragEnter(event)"\
+    ondragleave="dragExit(event)">\
+    <header class="header">{{ column.title }}</header>\
+    <card v-for="card in column.cards" v-bind:card="card" v-bind:key="card.id"></card>\
+    </div>'
+})
+
+Vue.component("card", {
+    props: ["card"],
+    template: '<div :id="card.id" \
+    class="card" \
+    draggable="true" \
+    ondragstart="dragStart(event)">\
+    {{ card.title }}\
+    </div>'
+})
+
+
+var vm = new Vue({
+    el: "#app-board",
+    data: {
+        board: board
+    }
+})
